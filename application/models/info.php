a424177b83507f3c8f20f55e1ef124198c6c64b1
GTartari <redas.gt@gmail.com>
2016-06-21 23:32:20 -0400

    agregar consultas
    se agregaron las consultas entregadas en la tarea2

---
+++ application/models/info.php
@@ -0,0 +1,67 @@
+<?php
+	class info extends CI_Model {
+
+    function __construct(){
+        // Call the Model constructor
+        parent::__construct();
+    }
+
+    function agregarJugador($nombre, $apellido, $rut, $sexo, $usuario, $password, $email, $fecha_registro, $ultimo_ingreso, $status){
+        
+        $sql = "INSERT INTO `jugador` (`idjugador`, `nombre_jugador`, `apellido_paterno`, `apellido_materno`,
+                `rut`) VALUES (NULL, '".$nombre."', '".$apellido."', '".$rut."', '".$sexo."', '".$usuario."', 
+                '".$password."', '".$email."', '".$fecha_registro."', '".$ultimo_ingreso."', '".$status."')";
+        $qry = $this->db->query($sql);
+    }
+
+    function agregarJugadorTorneo($rut, $nombreTorneo){
+        
+        $sql = "INSERT INTO jugador_has_torneo (`jugador_idjugador`, `torneo_idtorneo`)
+                SELECT idjugador,idtorneo FROM jugador INNER JOIN torneo
+                WHERE rut='".$rut."' AND nombre_torneo='".$nombreTorneo."'";
+        $qry = $this->db->query($sql);
+    }
+   	
+    function actualizarJugador($nombre, $apellido, $rut){
+        
+        $sql = "UPDATE jugador SET nombre_jugador='".$nombre."', apellido='".$apellido."' WHERE rut=".$rut;
+        $qry = $this->db->query($sql);
+    }
+
+    function eliminarNoticia($idNoticia){
+        
+        $sql = "DELETE FROM `noticia` WHERE `noticia`.`idnoticia` = ".$idNoticia;
+        $qry = $this->db->query($sql);
+    }
+
+    function eliminarSocio($idSocio){
+        
+        $sql = "DELETE FROM `socio` WHERE `socio`.`idsocio` = ".$idSocio;
+        $qry = $this->db->query($sql);
+    }
+
+    function numeroTorneosJugador($idJugador){
+        
+        $sql = "SELECT COUNT(*) FROM `jugador_has_torneo` WHERE jugador_idjugador=".$idJugador;
+        $qry = $this->db->query($sql);
+        if($qry->num_rows() > 0){
+            return $qry->result();
+        }
+        else{
+            return null;
+        }
+    }
+
+    function numeroSocios(){
+        
+        $sql = "SELECT COUNT(*) FROM `socio`";
+        $qry = $this->db->query($sql);
+        if($qry->num_rows() > 0){
+            return $qry->result();
+        }
+        else{
+            return null;
+        }
+    }
+}
+?>
