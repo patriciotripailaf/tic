319d212bce15393b249c9aa62390d3e94b9cfec2
patriciotripailaf <patricio.tripailaf@gmail.com>
2016-06-25 23:03:26 -0400

    espacios registro

--- application/views/registro_usuario.php
+++ application/views/registro_usuario.php
@@ -11,36 +11,36 @@
 
 		echo form_open_multipart('control_registro/nuevo_registro')."\n"; 
 		
-		echo form_label('Nombre:', 'nombre')."\n";
+		echo form_label('Nombre:', 'nombre')."<br>";
 		echo form_input('nombre')."<br>";
 		
-		echo form_label('Apellido:', 'apellido')."\n";
+		echo form_label('Apellido:', 'apellido')."<br>";
 		echo form_input('apellido')."<br>";
 
-		echo form_label('Rut:', 'rut')."\n";
+		echo form_label('Rut:', 'rut')."<br>";
 		echo form_input('rut')."<br>";
 
 		$options = array(
         'hombre' => 'Hombre',
         'mujer' => 'Mujer'
 		);
-		echo form_label('Sexo:', 'sexo')."\n";
-		echo form_dropdown('sexo', $options)."<br>";
+		echo form_label('Sexo:', 'sexo')."<br>";
+		echo form_dropdown('sexo', $options)."<br>"."<br>";
 		?>
 		<input id='foto-animal'    accept="image/*" name='foto' type='file' required> <br>
 		<?php	
-		echo form_label('Correo:', 'correo')."\n";
+		echo form_label('Correo:', 'correo')."<br>";
 		echo form_input('correo')."<br>";
 
-		echo form_label('Usuario:', 'usuario')."\n";
+		echo form_label('Usuario:', 'usuario')."<br>";
 		echo form_input('username')."<br>";
 		
-		echo form_label('Contraseña:', 'pass')."\n";
-		echo form_password('password')."<br>";
+		echo form_label('Contraseña:', 'pass')."<br>";
+		echo form_password('password')."<br>"."<br>";
 		
 	
 		echo form_submit('enviar', 'Registrar');
-		echo form_close();
+		echo form_close()."<br>";
 		echo anchor('', 'Volver') 
 	?>
 
